21ec47ec79b99279bef8e4848252e53cbfeaf11e
최은철 <wndqhrgkdlel@gmail.com>
2024-03-19 11:17:22 +0900

    메타데이터 예시 개발자도구에서 title 태그 아래에 작성됨

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -1,6 +1,12 @@
+import { Helmet } from 'react-helmet';
+
 function Title() {
     return (
     <div>
+        <Helmet>
+            <title> 메타 데이터 제목 </title>
+            <meta name='description' content='메타 데이터 테스트입니다.'></meta>
+        </Helmet>
         <h2>My Todo List</h2>
     </div>
 )}

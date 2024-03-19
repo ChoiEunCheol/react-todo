ca9a0c28e69b6e2df5d74f6a48869923485263d7
최은철 <wndqhrgkdlel@gmail.com>
2024-03-19 11:33:27 +0900

    <title> 태그가 탭 제목을 바꾸는 것이었음

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -4,7 +4,6 @@ function Title() {
     return (
     <div>
         <Helmet>
-            <title> 메타 데이터 제목 </title>
             <meta name='description' content='메타 데이터 테스트입니다.'></meta>
         </Helmet>
         <h2>My Todo List</h2>

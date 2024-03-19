239f901bfec78930f026b79d2d3d96c0191f3cee
최은철 <wndqhrgkdlel@gmail.com>
2024-03-19 15:58:22 +0900

    List 컴포넌트

--- src/component/List.tsx
+++ src/component/List.tsx
@@ -1,7 +1,16 @@
 function List() {
-  const test = window.localStorage.getItem("todo-List");
-
-  return <div>{test}</div>;
-}
-
-export default List;
+    const test = window.localStorage.getItem("todo-List");
+    const test2 = test ? JSON.parse(test) : [];
+  
+    return (
+      <div>
+          <ul>
+              {test2.map((element:string, index:string) => (
+                  <li key={index}>{element}</li>
+              ))}
+          </ul>
+      </div>
+    )
+  }
+  
+  export default List;
